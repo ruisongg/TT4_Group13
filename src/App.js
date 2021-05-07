deea0c79b6872746e474355cbbe60314c4a32b61
Ming <mtan089@e.ntu.edu.com>
2021-05-07 13:12:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import './App.css';
-import Login from "./pages/login.js";
-import Nav from "./nav/nav.js";
-import {BrowserRouter, Route} from 'react-router-dom';
-import BalancePage from './pages/BalancePage';
-import TransferPage from './pages/TransferPage';
-import HistoryPage from './pages/HistoryPage';
+import React, {useState} from 'react';
+import Login from './components/login';
+import Form from './components/form';
+import "./App.scss";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Nav/>
-        <main className="form-signin">
-          <Route path="/" exact component={Login}/>
-          <Route path="/balance" exact component={BalancePage}/>
-          <Route path="/transfer" exact component={TransferPage}/>
-          <Route path="/history" exact component={HistoryPage}/>
-        </main>
-      </BrowserRouter>
-    </div>
+    <>
+    
+      {isAuthenticated ? (<Form setIsAuthenticated={setIsAuthenticated}/>):(<Login setIsAuthenticated={setIsAuthenticated}/>) }
+    </>
   );
 }
 
 export default App;
+
+
+
+
