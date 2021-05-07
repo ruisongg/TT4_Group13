68b20b03b2ce1cc35a4e618ee13d1c95849ffeaf
SKEE006 <78146627+SKEE006@users.noreply.github.com>
2021-05-07 15:22:24 +0800

    sometimes array, sometimes no

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 function HistoryPage() {
 
     const [user, setUser] = useState({});
+    const [hist, setHist] = useState([])
 
     useEffect(() => {
         setUser(JSON.parse(localStorage.getItem("user")))
@@ -18,27 +19,35 @@ function HistoryPage() {
                 body: `{ "custID": ${user.custID},"accountKey": "${user.accountKey}" }`
                 }).then(respond => respond.json())
                 .then(data => {
-                                console.log(JSON.stringify(data))
+                              //  console.log(JSON.stringify(data))
+                                setHist([data]);
+                                console.log(hist)
                                 });
         }
     }, [user])
 
     return (
         <div>
-          <h1>{user.lastName}'s History</h1>
+          <h1>{user.firstName}'s History</h1>
+        
           <table class="table table-striped table-bordered table-condensed">
             <thead>
               <tr>
+                <th>Date</th>
+                <th>Payee</th>
                 <th>Description</th>
                 <th>Type</th>
                 <th>Amount</th>
               </tr>
             </thead>
+
             <tbody>
               <tr ng-repeat="row in transactionList | filter: transType">
-                <td ng-bind="row.description"></td>
-                <td ng-bind="row.type"></td>
-                <td ng-bind="row.amount | currency"></td>
+                <td ng-bind="row.date"> date </td>
+                <td ng-bind="row.payee"> payee </td>
+                <td ng-bind="row.description"> desc </td>
+                <td ng-bind="row.type"> egift </td>
+                <td ng-bind="row.amount | currency"> amt </td>
               </tr>
             </tbody>
           </table>
